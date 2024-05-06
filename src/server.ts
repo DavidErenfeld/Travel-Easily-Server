b5eea72d82a15d71dc64dee2e9970fa461c63e29
David Erenfeld <1020dudu@gmail.com>
2024-05-06 21:28:41 +0300

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,21 @@ import fs from "fs";
 import https from "https";
 import http from "http";
 
-if (process.env.NODE_ENV === "production") {
-  const httpsOptions = {
-    key: fs.readFileSync(process.env.SSL_KEY_PATH),
-    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
-  };
-  https.createServer(httpsOptions, app).listen(process.env.HTTPS_PORT, () => {
-    console.log(
-      `Server running in production on https://enigmatic-island-56921-258869278475.herokuapp.com/${process.env.HTTPS_PORT}`
-    );
-  });
-} else {
-  http.createServer(app).listen(process.env.PORT || 3000, () => {
-    console.log(
-      `Server running in development on https://enigmatic-island-56921-258869278475.herokuapp.com/${
-        process.env.PORT || 3000
-      }`
-    );
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   const httpsOptions = {
+//     key: fs.readFileSync(process.env.SSL_KEY_PATH),
+//     cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+//   };
+//   https.createServer(httpsOptions, app).listen(process.env.HTTPS_PORT, () => {
+//     console.log(
+//       `Server running in production on https://enigmatic-island-56921-258869278475.herokuapp.com/${process.env.HTTPS_PORT}`
+//     );
+//   });
+// } else {
+http.createServer(app).listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server running in development on https://enigmatic-island-56921-258869278475.herokuapp.com/${
+      process.env.PORT || 3000
+    }`
+  );
+});
